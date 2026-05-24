4f39bb0edbf3794d44f198bde133988d5bea42cb
c4t <c4t@DESKTOP-C4T>
2026-05-24 11:57:19 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace RawJumps
+{
+    [BepInPlugin("xyz.flyinc4t.rawjumps", "RawJumps", "1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        [HarmonyPatch(typeof(GorillaLocomotion.GTPlayer), "velocityLimit")]
+        void Awake()
+        {
+            GameObject player = GorillaLocomotion.GTPlayer.Instance;
+            if (GorillaLocomotion.GTPlayer.Instance != null)
+            {
+                GorillaLocomotion.GTPlayer.Instance.velocityLimit = 0;
+            }
+        }
+    }
+}
