9e31a94b17233df4457342c3f420352949413de7
c4t <c4t@DESKTOP-C4T>
2026-05-24 17:30:53 +0200

    Interval-based updating instead of initial

--- Plugin.cs
+++ Plugin.cs
@@ -8,14 +8,24 @@ namespace RawJumps
     [BepInPlugin("xyz.flyinc4t.rawjumps", "RawJumps", "1.0")]
     public class Plugin : BaseUnityPlugin
     {
-        [HarmonyPatch(typeof(GorillaLocomotion.GTPlayer), "velocityLimit")]
-        void Awake()
+        private float _updateTimer;
+
+        [HarmonyPatch(typeof(GorillaLocomotion.GTPlayer), "velocityLimit" + "slideVelocityLimit")]
+        private void UpdatePlayer()
         {
-            GameObject player = GorillaLocomotion.GTPlayer.Instance;
-            if (GorillaLocomotion.GTPlayer.Instance != null)
+            GorillaLocomotion.GTPlayer player = GorillaLocomotion.GTPlayer.Instance;
+
+            if (player != null)
             {
-                GorillaLocomotion.GTPlayer.Instance.velocityLimit = 0;
+                player.velocityLimit = 0;
+                player.slideVelocityLimit = 0;
             }
+        }
+
+        void Update()
+        {
+            _updateTimer += Time.deltaTime;
+            if (_updateTimer > 7) UpdatePlayer();
         }
     }
 }
